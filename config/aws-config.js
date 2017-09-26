b1cf5c3fc461db7a34a04684b9c7804a9f583db9
Dushyantha Samanbaddha <dushsb@gmail.com>
2017-09-26 17:31:52 +0800

    Update aws-config.js

--- config/aws-config.js
+++ config/aws-config.js
@@ -4,6 +4,6 @@
 // constants configured with the values of your project. You should use that
 // file instead of this one. This is meant to show a sample of that file.
 
-const aws_bots_config = '[{"name":"LibertyChatBot","alias":"prod","description":"Bot to order flowers.","bot-template":"bot-flowers","commands-help":["I would like to pick up flowers","I would like to order some flowers","Buy flowers"],"region":"us-east-1"}]';
-const aws_cognito_identity_pool_id = 'us-east-1:8f22616d-57db-40dd-ac83-1a046ad20f72';
+const aws_bots_config = '[{"name":"","alias":"prod","description":"Bot to order flowers.","bot-template":"bot-flowers","commands-help":["I would like to pick up flowers","I would like to order some flowers","Buy flowers"],"region":"us-east-1"}]';
+const aws_cognito_identity_pool_id = '';
 const aws_cognito_region = 'us-east-1';
